1786fc79cd29b47b78c95f05c5f124ff5b0d4555
marciof2 <marcinsilva26@gmail.com>
2023-09-03 12:20:04 -0300

    Start Project

---
+++ lib/poke_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class PokeView extends StatefulWidget {
+  const PokeView({Key? key}) : super(key: key);
+
+  @override
+  State<PokeView> createState() => _PokeViewState();
+}
+
+class _PokeViewState extends State<PokeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
